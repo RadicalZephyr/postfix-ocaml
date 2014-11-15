87013f9a7f3cebb60227e9e748b78151f3a48c7d
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-15 17:30:48 -0500

    Specify a compiler to install as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
 # Install llvm 3.5
 apt-get install -y llvm-3.5 llvm-3.5-dev llvm-3.5-runtime llvm-3.5-doc llvm-3.5-tools llvm-3.5-examples
 
-opam init --no-setup
+opam init --no-setup --compiler 4.02.1
 
 eval `opam config env`
 
