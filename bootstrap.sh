d35f27e9368b4e81c4130a64d8a203f850cda04d
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-15 16:58:57 -0500

    Setup with a newer version of opam

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,18 @@
 
 set -e
 
+add-apt-repository ppa:avsm/ppa
+
 apt-get update
 
-# Install opam and llvm 3.5
-apt-get install -y opam llvm-3.5 llvm-3.5-dev llvm-3.5-runtime llvm-3.5-doc llvm-3.5-tools llvm-3.5-examples
+# Install opam and ocaml
+apt-get install -y ocaml ocaml-native-compilers camlp4-extra opam
+
+# Install llvm 3.5
+apt-get install -y llvm-3.5 llvm-3.5-dev llvm-3.5-runtime llvm-3.5-doc llvm-3.5-tools llvm-3.5-examples
+
+opam init --no-setup
 
 opam update
 
-opam install core core_extended utop llvm
+opam install -y core core_extended utop llvm
