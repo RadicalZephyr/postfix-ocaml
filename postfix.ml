a20c1329400bc9e9c31220488652a10eafed565e
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-15 19:04:14 -0500

    Enhance program with a usage message

--- postfix.ml
+++ postfix.ml
@@ -1,3 +1,17 @@
 open Core.Std
 
-let () = print_string "Hello World!"
+let print_usage pname =
+  printf "usage: %s <program>" pname
+
+let () =
+  match (Array.to_list Sys.argv) with
+  (* If we don't get the right number of arguments, print out the usage
+  message. *)
+  | [] ->
+     print_usage ""
+  | pname :: [] ->
+     print_usage pname
+  | _ :: program :: [] ->
+     ()
+  | pname :: _ :: _ ->
+     print_usage pname
